d9cbb4a4edce87f9404a6a671f756301f1afe036
keserizzet <izzetfurkankeserwtk@gmail.com>
2025-03-12 11:05:02 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // 🚀 VerceL için doğru URL yolunu sağlar
+  base: "/",
   build: {
-    outDir: "dist", // VerceL için doğru build dizini
+    outDir: "dist",
   },
 });
