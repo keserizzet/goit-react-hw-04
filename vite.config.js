f1dc6554ef2be3662005af72c768c8342c5d830e
keserizzet <izzetfurkankeserwtk@gmail.com>
2025-03-12 21:14:32 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // ✅ Bu ayar Vercel için çok önemli!
+  base: "./",
   build: {
     outDir: "dist",
     rollupOptions: {
